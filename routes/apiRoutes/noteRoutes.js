92bb20297084761d1d40f9f79682966b753e0b24
Kyle <kylejmbaylis@gmail.com>
2022-05-07 12:57:30 -0400

    started noteRoutes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const { /* functions will go here */ } = require('../../lib/notes');
+const { notes } = require('../../Develop/db/db.json');
+
+router.get(/* notes */)
+
+router.post( /* notes */)
